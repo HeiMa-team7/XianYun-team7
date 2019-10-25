5ba402c102ff77f509ea768de05d2f44b0d3a3d5
Will-Z-fighting <53569286+Will-Z-fighting@users.noreply.github.com>
2019-10-25 09:27:33 +0800

    修改地图坐标为南京市景点酒店

--- components/hotel/godMap.vue
+++ components/hotel/godMap.vue
@@ -43,26 +43,26 @@ export default {
             AMap.service(["AMap.PlaceSearch"], function() {
                 //构造地点查询类
                 var placeSearch = new AMap.PlaceSearch({ 
-                    pageSize: 5, // 单页显示结果条数
+                    pageSize: 15, // 单页显示结果条数
                     pageIndex: 1, // 页码
-                    /* city: "010", */ // 兴趣点城市
+                    city: "025", // 兴趣点城市
                     citylimit: true,  //是否强制限制在设置的城市内搜索
                     map: map, // 展现结果的地图实例
                     panel: "panel", // 结果列表将在此容器中进行展示。
                     autoFitView: true // 是否自动调整地图视野使绘制的 Marker点都处于视口的可见范围
                 });
                 //关键字查询
-                placeSearch.search('高淳县淳溪镇');
+                placeSearch.search('酒店&景点');
                 
             });
         }
     },
 
     mounted(){
-
+        const {city,id} = this.$route.query;
         setTimeout(() => {
             this.$axios({
-                url: "/hotels?city=" + 87
+                url: `/hotels?city=${city}&id=${id}`
             }).then(res => {
                 this.location = res.data.data[0].location;
                 console.log( this.location);
